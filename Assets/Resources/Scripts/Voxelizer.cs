f08a076fda08a04fa529fc4c35511be70bd0ddea
David Ordoñez <vash3.2@gmail.com>
2019-04-03 23:35:58 +0200

    Bevel Cube

--- Assets/Resources/Scripts/Voxelizer.cs
+++ Assets/Resources/Scripts/Voxelizer.cs
@@ -58,6 +58,12 @@ public class Voxelizer : MonoBehaviour
 		voxelizedMetallicSmoothness.filterMode = FilterMode.Point;
 	}
 
+	void createBevelCubeMesh()
+	{
+		var meshFilter = Resources.Load<MeshFilter>("Meshes/voxel_cube");
+		this.cube = meshFilter.sharedMesh;
+	}
+
 	void createCubeMesh()
 	{
 		Vector3 p0 = new Vector3(-.5f, -.5f, .5f);
@@ -74,19 +80,19 @@ public class Voxelizer : MonoBehaviour
 		{
 			// Bottom
 			p0, p1, p2, p3,
- 
+
 			// Left
 			p7, p4, p0, p3,
- 
+
 			// Front
 			p4, p5, p1, p0,
- 
+
 			// Back
 			p6, p7, p3, p2,
- 
+
 			// Right
 			p5, p6, p2, p1,
- 
+
 			// Top
 			p7, p6, p5, p4
 		};
@@ -95,23 +101,23 @@ public class Voxelizer : MonoBehaviour
 			// Bottom
 			3, 1, 0,
 			3, 2, 1,			
- 
+
 			// Left
 			3 + 4 * 1, 1 + 4 * 1, 0 + 4 * 1,
 			3 + 4 * 1, 2 + 4 * 1, 1 + 4 * 1,
- 
+
 			// Front
 			3 + 4 * 2, 1 + 4 * 2, 0 + 4 * 2,
 			3 + 4 * 2, 2 + 4 * 2, 1 + 4 * 2,
- 
+
 			// Back
 			3 + 4 * 3, 1 + 4 * 3, 0 + 4 * 3,
 			3 + 4 * 3, 2 + 4 * 3, 1 + 4 * 3,
- 
+
 			// Right
 			3 + 4 * 4, 1 + 4 * 4, 0 + 4 * 4,
 			3 + 4 * 4, 2 + 4 * 4, 1 + 4 * 4,
- 
+
 			// Top
 			3 + 4 * 5, 1 + 4 * 5, 0 + 4 * 5,
 			3 + 4 * 5, 2 + 4 * 5, 1 + 4 * 5,
@@ -162,7 +168,8 @@ public class Voxelizer : MonoBehaviour
 		computeWorldToUnitCubeMatrix();
 		obtainMeshes();
 		initShaders();
-		createCubeMesh();
+		//createCubeMesh();
+		createBevelCubeMesh();
 	}
 
 	void clearVoxelizedScene()
